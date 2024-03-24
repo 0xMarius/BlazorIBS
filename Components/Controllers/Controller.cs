d8117f78093516fe4a08023adbbfd6efd48f8759
0xmarius <tech@marmakas.lt>
2024-03-24 20:12:53 +0200

    Refactor POST request to generate 'saskaitosNr' automatically upon receiving the request

--- Components/Controllers/Controller.cs
+++ Components/Controllers/Controller.cs
@@ -1,23 +1,25 @@
-﻿// Example controller class for a sample API
-using Microsoft.AspNetCore.Mvc;
+﻿using System.Text;
 using System.Data;
+using System.Security.Cryptography;
+using System.Net.NetworkInformation;
+using static System.Net.Mime.MediaTypeNames;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
 namespace DatabaseWork
 {
     [ApiController]
-    [Route("api/controller")]
-    public class SampleController : ControllerBase
+    [Route("api/")]
+    public class BaseController : ControllerBase
     {
         private readonly OracleService _oracleService;
-        public SampleController(OracleService oracleService)
+        public BaseController(OracleService oracleService)
         {
             _oracleService = oracleService;
         }
 
-
-        // GET: api/controller/datatable
-        [HttpGet("datatable")]
+        // GET: api/
+        [HttpGet]
         public ActionResult<string> Get()
         {
             DataTable dataTable = _oracleService.getDataTable("SELECT * FROM Saskaitos ORDER BY pavarde");
@@ -29,24 +31,45 @@ namespace DatabaseWork
             return Content(jsonData, "application/json");
         }
 
-        // POST: api/controller/process   
-        [HttpPost("process")]
-        public ActionResult<string> ProcessData([FromBody] string data)
+        // POST: api/create
+        [HttpPost("create")]
+        public ActionResult<string> CreateNewAccount([FromBody] string data)
         {
-            // Deserialize the JSON string into a dynamic object
+            // deserialize the JSON string into a dynamic object
             dynamic jsonData = JsonConvert.DeserializeObject(data);
 
-            // Access individual properties
+            // access individual properties
             string vardas = jsonData.vardas;
             string pavarde = jsonData.pavarde;
             string asmensKodas = jsonData.asmensKodas;
-            string saskaitosNr = jsonData.saskaitosNr;
+            string saskaitosNr = GenerateSaskaitosNr(asmensKodas);
 
-            string insertQuery = $"INSERT INTO Saskaitos(vardas, pavarde, asmens_kodas, saskaitos_numeris, lesos) VALUES('{vardas}', '{pavarde}', '{asmensKodas}', '{saskaitosNr}', 0.00)";
-            _oracleService.getDataTable(insertQuery);
+            string numberOfAccounts = $"SELECT COUNT(*) FROM Saskaitos WHERE asmens_kodas = {asmensKodas}";
 
-            // Return the JSON data as the response
-            return "Account created successfully.";
+            // if the count of rows where the value in the asmens_kodas column matches asmensKodas equals 0
+            if (_oracleService.getValue(numberOfAccounts) == 0)
+            {
+                string insertQuery = $"INSERT INTO Saskaitos(vardas, pavarde, asmens_kodas, saskaitos_numeris, lesos) VALUES('{vardas}', '{pavarde}', '{asmensKodas}', '{saskaitosNr}', 0.00)";
+                _oracleService.getDataTable(insertQuery);
+                // Return the JSON data as the response
+                return "Account creation successful.";
+            }
+            else
+            {
+                return "Account creation failed: already exists.";
+            }
+        }
+        private string GenerateSaskaitosNr(string asmensKodas)
+        {
+            byte[] hashBytes;
+
+            // TO-DO: sreplace with stronger hashing + random salting values
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(asmensKodas));
+            }
+
+            return Convert.ToBase64String(hashBytes);
         }
     }
 }
